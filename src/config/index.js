e9c1ab59ed03873b28aa635a854445984d5eabb3
José Fernando Gómez <jofergoz92@gmail.com>
2026-07-23 09:45:36 -0300

    modificacion-datos

--- src/config/index.js
+++
@@ -1,1 +0,0 @@
-export { config, default } from './environment.config.js';
--- src/routes/sessions.router.js
+++ src/routes/sessions.router.js
@@ -1,12 +1,1 @@
-import { Router } from 'express';
-import { sessionsController } from '../controllers/sessions.controller.js';
 
-const router = Router();
-
-/**
- * @route GET /api/sessions
- * @desc Estado inicial de la estructura de sesiones
- */
-router.get('/', sessionsController.getSessionStatus);
-
-export default router;
