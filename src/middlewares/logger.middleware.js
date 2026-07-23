52e21df8cb11fc9eb5544ce3dbf8b1eb9cbae78f
José Fernando Gómez <jofergoz92@gmail.com>
2026-07-23 10:02:08 -0300

    reestructura

--- src/middlewares/logger.middleware.js
+++
@@ -1,9 +0,0 @@
-import { logger } from '../utils/logger.utils.js';
-
-/**
- * Middleware para registrar peticiones HTTP entrantes.
- */
-export const requestLogger = (req, res, next) => {
-  logger.info(`${req.method} ${req.originalUrl}`);
-  next();
-};
